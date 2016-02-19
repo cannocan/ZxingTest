7f335825a10c15ef422c25b907ecf3e46cf82a40
xiaohuhu <512999727@qq.com>
2016-02-19 16:55:36 +0800

    ceshi

--- app/src/main/java/me/test/zxing/Main2Activity.java
+++ app/src/main/java/me/test/zxing/Main2Activity.java
@@ -13,7 +13,5 @@ public class Main2Activity extends Activity {
 
         setContentView(R.layout.activity_main2);
         TextView tv = (TextView) findViewById(R.id.abc);
-
-        tv.setText(getIntent().getStringExtra("result"));
     }
 }
